c396e8e23e4576762ebb4dfb8a12955a28c15ac7
FelipeSSac <felipe_adoubs@outlook.com>
2021-07-20 20:16:07 -0300

    aula 13

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 import Post from './Post';
 
 export default function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random() ,title: 'Title#01', subtitle: 'subtitle#01', likes: 20, read: true },
+    { id: Math.random() ,title: 'Title#02', subtitle: 'subtitle#02', likes: 10, read: false },
+    { id: Math.random() ,title: 'Title#03', subtitle: 'subtitle#03', likes: 50, read: false },
+  ]);
+
+  function handleRefresh() {
+    setPosts((prevState) => [
+      ...prevState,
+      { 
+        id: Math.random() ,
+        title: `Title#${prevState.length+1}`, 
+        subtitle: `subtitle#${prevState.length+1}`, 
+        likes: 50
+      }
+    ]);
+  }
+
+  function handleRemove(postId) {
+    setPosts((prevState) => (
+      prevState.filter((post) => post.id !== postId)
+    ))
+  }
+  
   return(
     <>
       <Header>
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh} >Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
 
-      <Post 
-        title="Noticia 1"
-        subtitle="Subtitulo 1"
-        likes={20}
-      />
-      <Post 
-        title="Noticia 2"
-        subtitle="Subtitulo 2"
-        likes={2}
-      />
-      <Post 
-        title="Noticia 3"
-        subtitle="Subtitulo 3"
-        likes={8}
-      />
+      {posts.map(post => (
+        <Post 
+          key={ post.title }
+          likes = { post.likes }
+          onRemove={ handleRemove }
+          post = {post}
+        />
+      ))}
     </>
   )
 };
--- src/Post.js
+++ src/Post.js
@@ -5,10 +5,18 @@ export default function Post(props) {
   return (
     <>
     <article>
-        <strong>{props.title}</strong><br />
-        <small>{props.subtitle}</small>
+        <strong>
+          {
+            props.post.read ? 
+              <s>{props.post.title}</s>
+            :
+              props.post.title
+          }
+        </strong><br />
+        <small>{props.post.subtitle}</small>
         <br />
-        likes: {props.likes}
+        <button onClick={() => props.onRemove(props.post.id)}>remover</button>
+        likes: {props.post.likes}
       </article>
       <br />
     </>
@@ -16,9 +24,12 @@ export default function Post(props) {
 }
 
 Post.PropTypes = {
-  likes: PropTypes.number.isRequired,
+  onRemove: PropTypes.func.isRequired,
   post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    read: PropTypes.bool.isRequired
   }).isRequired,
 }
